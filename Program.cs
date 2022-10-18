f6e2766073baa8880713a8f45b66d1dfd0bb4b53
AlexArchanfel <lentao_07@mail.ru>
2022-10-18 03:14:13 +0300

    Home25

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа А и В и возводит А в натуральную степень В 
+int Roma (int A, int B)
+{
+    int Rom = 1;
+    int i = 0;
+
+    while (i < B)
+    {
+        Rom = Rom * A;
+        i = i + 1;
+    } 
+    return Rom;
+}
+Console.Clear();
+Console.Write ("Введите первое число: ");
+int A = int.Parse (Console.ReadLine ()!);
+Console.Write ("Введите второе число: ");
+int B = int.Parse (Console.ReadLine ()!);
+Console.WriteLine (Roma(A, B));
